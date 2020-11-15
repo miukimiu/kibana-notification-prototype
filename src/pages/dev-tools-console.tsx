a1c5e4c4f65da814f816d5e71ff8c893e396f0b3
cchaos <caroline.horn@elastic.co>
2020-11-15 14:55:21 -0500

    Updated Dev tools

--- src/pages/dev-tools-console.tsx
+++ src/pages/dev-tools-console.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Layout from '../components/layout';
 
-import { EuiFlexGroup, EuiFlexItem, EuiBreadcrumb } from '@elastic/eui';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiBreadcrumb,
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentBody,
+  EuiPageHeader,
+  EuiPageHeaderSection,
+  EuiSuperDatePicker,
+  EuiTab,
+  EuiTabs,
+  EuiBottomBar,
+  EuiButton,
+} from '@elastic/eui';
 // @ts-ignore
 import console_img from '../images/Dev Tools - Console.png';
 
@@ -17,16 +32,43 @@ export default () => (
     <Helmet>
       <title>Console | Dev Tools | Kibana 8 Prototype</title>
     </Helmet>
-
-    <EuiFlexGroup justifyContent="center" gutterSize="none">
-      <EuiFlexItem grow={false}>
-        <img
-          className="pageScreenshot pageScreenshot--fullWidth"
-          alt="Dev Tools / Console"
-          width={1175}
-          src={console_img}
-        />
-      </EuiFlexItem>
-    </EuiFlexGroup>
+    <EuiPage>
+      <EuiPageBody>
+        <EuiPageHeader>
+          <EuiPageHeaderSection>
+            <EuiTabs display="condensed">
+              <EuiTab isSelected>Console</EuiTab>
+              <EuiTab>Search profiler</EuiTab>
+              <EuiTab>Grok debugger</EuiTab>
+              <EuiTab>Painless lab</EuiTab>
+            </EuiTabs>
+          </EuiPageHeaderSection>
+        </EuiPageHeader>
+        {/** TODO: How to afford for bottom bar displacement outside of body */}
+        <EuiPageContent style={{ paddingBottom: 72 }}>
+          <EuiPageContentBody>
+            <img
+              className="pageScreenshot pageScreenshot--fullWidth"
+              alt="Dev Tools / Console"
+              width={1341}
+              src={console_img}
+            />
+          </EuiPageContentBody>
+        </EuiPageContent>
+      </EuiPageBody>
+    </EuiPage>
+    <EuiBottomBar affordForDisplacement={false} paddingSize="s">
+      <EuiButton size="s" color="ghost" minWidth={0}>
+        History
+      </EuiButton>
+      &emsp;
+      <EuiButton size="s" color="ghost" minWidth={0}>
+        Settings
+      </EuiButton>
+      &emsp;
+      <EuiButton size="s" color="ghost" minWidth={0}>
+        Help
+      </EuiButton>
+    </EuiBottomBar>
   </Layout>
 );
